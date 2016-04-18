87b7ad4186a34066cb9102ac052be43f0a87ce2e
raidi01 <raidi01@8259.local>
2016-04-18 15:00:26 -0500

    project 4 Q1,2

---
+++ Smashtag/Smashtag/TweetTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by raidi01 on 4/2/16.
+//  Copyright © 2016 Luther College. All rights reserved.
+//
+
+import Cocoa
+
+class TweetTableViewController: UITableViewController {
+
+}
