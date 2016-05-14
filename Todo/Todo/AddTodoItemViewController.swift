aea00c1fd7a42bd9ad5b233717cfa95bf4584136
raidi01 <raidi01@8259.local>
2016-05-14 11:34:40 -0500

    todo app

--- Todo/Todo/AddTodoItemViewController.swift
+++ Todo/Todo/AddTodoItemViewController.swift
@@ -2,51 +2,12 @@
 //  AddTodoItemViewController.swift
 //  Todo
 //
-//  Created by raidi01 on 5/5/16.
+//  Created by raidi01 on 5/9/16.
 //  Copyright © 2016 Luther College. All rights reserved.
 //
 
-import UIKit
+import Cocoa
 
 class AddTodoItemViewController: UIViewController {
-
-    @IBOutlet weak var textField: UITextField!
-    
-    @IBOutlet weak var doneButton: UIBarButtonItem!
-    
-    
-    var todoItem: TodoItem = TodoItem(itemName: "")
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if (self.textField.text != "") {
-            self.todoItem = TodoItem(itemName: self.textField.text!)
-        }
-    }
-    
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
-        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Todo/Todo/TodoItem.swift
+++ Todo/Todo/TodoItem.swift
@@ -2,19 +2,12 @@
 //  TodoItem.swift
 //  Todo
 //
-//  Created by raidi01 on 5/5/16.
+//  Created by raidi01 on 5/9/16.
 //  Copyright © 2016 Luther College. All rights reserved.
 //
 
-import UIKit
+import Cocoa
 
 class TodoItem: NSObject {
-    let itemName: String
-    var completed: Bool
-    
-    init(itemName: String, completed:Bool =  false) {
-        self.itemName = itemName
-        self.completed = completed
-    }
 
 }
